63d98b96da65196320e3f17081c58024297e9284
Quan Hua <quanhua92@gmail.com>
2015-10-22 14:48:47 +0700

    Add SurfaceTexture functions

--- recordvideotomp4/src/main/java/com/quan404/recordvideotomp4/MainActivity.java
+++ recordvideotomp4/src/main/java/com/quan404/recordvideotomp4/MainActivity.java
@@ -1,7 +1,7 @@
 package com.quan404.recordvideotomp4;
 
 import android.app.Activity;
-import android.content.res.Resources;
+import android.graphics.SurfaceTexture;
 import android.hardware.Camera;
 import android.media.MediaCodec;
 import android.media.MediaCodecInfo;
@@ -13,6 +13,9 @@ import android.opengl.EGLContext;
 import android.opengl.EGLDisplay;
 import android.opengl.EGLExt;
 import android.opengl.EGLSurface;
+import android.opengl.GLES11Ext;
+import android.opengl.GLES20;
+import android.opengl.Matrix;
 import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -25,6 +28,9 @@ import android.widget.Button;
 
 import java.io.File;
 import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+import java.nio.FloatBuffer;
 
 /**
  * Record the camera (no audio) to a MP4 file in sdcard.
@@ -71,6 +77,8 @@ public class MainActivity extends Activity {
      */
     private Camera mCamera;
 
+    private SurfaceTextureManager mStManager;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -88,7 +96,8 @@ public class MainActivity extends Activity {
 
                 prepareCamera(FRAME_WIDTH, FRAME_HEIGHT);
                 prepareEncoder(FRAME_WIDTH, FRAME_HEIGHT, BITRATE);
-
+                mInputSurface.makeCurrent();
+                prepareSurfaceTexture();
             }
         });
     }
@@ -385,6 +394,357 @@ public class MainActivity extends Activity {
                 throw new RuntimeException(msg + ": EGL error: 0x" + Integer.toHexString(error));
             }
         }
+    }
 
+    /**
+     * SurfaceTexture functions
+     */
+
+    /**
+     * Configures SurfaceTexture for camera preview.  Initializes mStManager, and sets the
+     * associated SurfaceTexture as the Camera's "preview texture".
+     * <p>
+     * Configure the EGL surface that will be used for output before calling here.
+     */
+    private void prepareSurfaceTexture() {
+        mStManager = new SurfaceTextureManager();
+        SurfaceTexture st = mStManager.getSurfaceTexture();
+        try {
+            mCamera.setPreviewTexture(st);
+        } catch (IOException ioe) {
+            throw new RuntimeException("setPreviewTexture failed", ioe);
+        }
+    }
+
+    /**
+     * Releases the SurfaceTexture.
+     */
+    private void releaseSurfaceTexture() {
+        if (mStManager != null) {
+            mStManager.release();
+            mStManager = null;
+        }
+    }
+    /**
+     * Manages a SurfaceTexture.  Creates SurfaceTexture and TextureRender objects, and provides
+     * functions that wait for frames and render them to the current EGL surface.
+     * <p>
+     * The SurfaceTexture can be passed to Camera.setPreviewTexture() to receive camera output.
+     */
+    private static class SurfaceTextureManager implements SurfaceTexture.OnFrameAvailableListener {
+        private SurfaceTexture mSurfaceTexture;
+        private STextureRender mTextureRender;
+
+        private Object mFrameSyncObject = new Object();     // guards mFrameAvailable
+        private boolean mFrameAvailable;
+
+        /**
+         * Creates instances of TextureRender and SurfaceTexture.
+         */
+        public SurfaceTextureManager() {
+            mTextureRender = new STextureRender();
+            mTextureRender.surfaceCreated();
+
+            if (DEBUG) Log.d(TAG, "textureID=" + mTextureRender.getTextureId());
+            mSurfaceTexture = new SurfaceTexture(mTextureRender.getTextureId());
+
+            // This doesn't work if this object is created on the thread that CTS started for
+            // these test cases.
+            //
+            // The CTS-created thread has a Looper, and the SurfaceTexture constructor will
+            // create a Handler that uses it.  The "frame available" message is delivered
+            // there, but since we're not a Looper-based thread we'll never see it.  For
+            // this to do anything useful, OutputSurface must be created on a thread without
+            // a Looper, so that SurfaceTexture uses the main application Looper instead.
+            //
+            // Java language note: passing "this" out of a constructor is generally unwise,
+            // but we should be able to get away with it here.
+            mSurfaceTexture.setOnFrameAvailableListener(this);
+        }
+
+        public void release() {
+            // this causes a bunch of warnings that appear harmless but might confuse someone:
+            //  W BufferQueue: [unnamed-3997-2] cancelBuffer: BufferQueue has been abandoned!
+            //mSurfaceTexture.release();
+
+            mTextureRender = null;
+            mSurfaceTexture = null;
+        }
+
+        /**
+         * Returns the SurfaceTexture.
+         */
+        public SurfaceTexture getSurfaceTexture() {
+            return mSurfaceTexture;
+        }
+
+        /**
+         * Replaces the fragment shader.
+         */
+        public void changeFragmentShader(String fragmentShader) {
+            mTextureRender.changeFragmentShader(fragmentShader);
+        }
+
+        /**
+         * Latches the next buffer into the texture.  Must be called from the thread that created
+         * the OutputSurface object.
+         */
+        public void awaitNewImage() {
+            final int TIMEOUT_MS = 2500;
+
+            synchronized (mFrameSyncObject) {
+                while (!mFrameAvailable) {
+                    try {
+                        // Wait for onFrameAvailable() to signal us.  Use a timeout to avoid
+                        // stalling the test if it doesn't arrive.
+                        mFrameSyncObject.wait(TIMEOUT_MS);
+                        if (!mFrameAvailable) {
+                            // TODO: if "spurious wakeup", continue while loop
+                            throw new RuntimeException("Camera frame wait timed out");
+                        }
+                    } catch (InterruptedException ie) {
+                        // shouldn't happen
+                        throw new RuntimeException(ie);
+                    }
+                }
+                mFrameAvailable = false;
+            }
+
+            // Latch the data.
+            mTextureRender.checkGlError("before updateTexImage");
+            mSurfaceTexture.updateTexImage();
+        }
+
+        /**
+         * Draws the data from SurfaceTexture onto the current EGL surface.
+         */
+        public void drawImage() {
+            mTextureRender.drawFrame(mSurfaceTexture);
+        }
+
+        @Override
+        public void onFrameAvailable(SurfaceTexture surfaceTexture) {
+            if (DEBUG) Log.d(TAG, "new frame available");
+            synchronized (mFrameSyncObject) {
+                if (mFrameAvailable) {
+                    throw new RuntimeException("mFrameAvailable already set, frame could be dropped");
+                }
+                mFrameAvailable = true;
+                mFrameSyncObject.notifyAll();
+            }
+        }
+    }
+    /**
+     * Code for rendering a texture onto a surface using OpenGL ES 2.0.
+     */
+    private static class STextureRender{
+        private static final int FLOAT_SIZE_BYTES = 4;
+        private static final int TRIANGLE_VERTICES_DATA_STRIDE_BYTES = 5 * FLOAT_SIZE_BYTES;
+        private static final int TRIANGLE_VERTICES_DATA_POS_OFFSET = 0;
+        private static final int TRIANGLE_VERTICES_DATA_UV_OFFSET = 3;
+        private final float[] mTriangleVerticesData = {
+                // X, Y, Z, U, Vo
+                -1.0f, -1.0f, 0, 0.f, 0.f,
+                1.0f, -1.0f, 0, 1.f, 0.f,
+                -1.0f,  1.0f, 0, 0.f, 1.f,
+                1.0f,  1.0f, 0, 1.f, 1.f,
+        };
+
+        private FloatBuffer mTriangleVertices;
+
+        private static final String VERTEX_SHADER =
+                "uniform mat4 uMVPMatrix;\n" +
+                        "uniform mat4 uSTMatrix;\n" +
+                        "attribute vec4 aPosition;\n" +
+                        "attribute vec4 aTextureCoord;\n" +
+                        "varying vec2 vTextureCoord;\n" +
+                        "void main() {\n" +
+                        "    gl_Position = uMVPMatrix * aPosition;\n" +
+                        "    vTextureCoord = (uSTMatrix * aTextureCoord).xy;\n" +
+                        "}\n";
+
+        private static final String FRAGMENT_SHADER =
+                "#extension GL_OES_EGL_image_external : require\n" +
+                        "precision mediump float;\n" +      // highp here doesn't seem to matter
+                        "varying vec2 vTextureCoord;\n" +
+                        "uniform samplerExternalOES sTexture;\n" +
+                        "void main() {\n" +
+                        "    gl_FragColor = texture2D(sTexture, vTextureCoord);\n" +
+                        "}\n";
+
+        private float[] mMVPMatrix = new float[16];
+        private float[] mSTMatrix = new float[16];
+
+        private int mProgram;
+        private int mTextureID = -12345;
+        private int muMVPMatrixHandle;
+        private int muSTMatrixHandle;
+        private int maPositionHandle;
+        private int maTextureHandle;
+
+        public STextureRender() {
+            mTriangleVertices = ByteBuffer.allocateDirect(
+                    mTriangleVerticesData.length * FLOAT_SIZE_BYTES)
+                    .order(ByteOrder.nativeOrder()).asFloatBuffer();
+            mTriangleVertices.put(mTriangleVerticesData).position(0);
+
+            Matrix.setIdentityM(mSTMatrix, 0);
+        }
+
+        public int getTextureId() {
+            return mTextureID;
+        }
+
+        public void drawFrame(SurfaceTexture st) {
+            checkGlError("onDrawFrame start");
+            st.getTransformMatrix(mSTMatrix);
+
+            // (optional) clear to green so we can see if we're failing to set pixels
+            GLES20.glClearColor(0.0f, 1.0f, 0.0f, 1.0f);
+            GLES20.glClear(GLES20.GL_DEPTH_BUFFER_BIT | GLES20.GL_COLOR_BUFFER_BIT);
+
+            GLES20.glUseProgram(mProgram);
+            checkGlError("glUseProgram");
+
+            GLES20.glActiveTexture(GLES20.GL_TEXTURE0);
+            GLES20.glBindTexture(GLES11Ext.GL_TEXTURE_EXTERNAL_OES, mTextureID);
+
+            mTriangleVertices.position(TRIANGLE_VERTICES_DATA_POS_OFFSET);
+            GLES20.glVertexAttribPointer(maPositionHandle, 3, GLES20.GL_FLOAT, false,
+                    TRIANGLE_VERTICES_DATA_STRIDE_BYTES, mTriangleVertices);
+            checkGlError("glVertexAttribPointer maPosition");
+            GLES20.glEnableVertexAttribArray(maPositionHandle);
+            checkGlError("glEnableVertexAttribArray maPositionHandle");
+
+            mTriangleVertices.position(TRIANGLE_VERTICES_DATA_UV_OFFSET);
+            GLES20.glVertexAttribPointer(maTextureHandle, 2, GLES20.GL_FLOAT, false,
+                    TRIANGLE_VERTICES_DATA_STRIDE_BYTES, mTriangleVertices);
+            checkGlError("glVertexAttribPointer maTextureHandle");
+            GLES20.glEnableVertexAttribArray(maTextureHandle);
+            checkGlError("glEnableVertexAttribArray maTextureHandle");
+
+            Matrix.setIdentityM(mMVPMatrix, 0);
+            GLES20.glUniformMatrix4fv(muMVPMatrixHandle, 1, false, mMVPMatrix, 0);
+            GLES20.glUniformMatrix4fv(muSTMatrixHandle, 1, false, mSTMatrix, 0);
+
+            GLES20.glDrawArrays(GLES20.GL_TRIANGLE_STRIP, 0, 4);
+            checkGlError("glDrawArrays");
+
+            // IMPORTANT: on some devices, if you are sharing the external texture between two
+            // contexts, one context may not see updates to the texture unless you un-bind and
+            // re-bind it.  If you're not using shared EGL contexts, you don't need to bind
+            // texture 0 here.
+            GLES20.glBindTexture(GLES11Ext.GL_TEXTURE_EXTERNAL_OES, 0);
+        }
+
+        /**
+         * Initializes GL state.  Call this after the EGL surface has been created and made current.
+         */
+        public void surfaceCreated() {
+            mProgram = createProgram(VERTEX_SHADER, FRAGMENT_SHADER);
+            if (mProgram == 0) {
+                throw new RuntimeException("failed creating program");
+            }
+            maPositionHandle = GLES20.glGetAttribLocation(mProgram, "aPosition");
+            checkLocation(maPositionHandle, "aPosition");
+            maTextureHandle = GLES20.glGetAttribLocation(mProgram, "aTextureCoord");
+            checkLocation(maTextureHandle, "aTextureCoord");
+
+            muMVPMatrixHandle = GLES20.glGetUniformLocation(mProgram, "uMVPMatrix");
+            checkLocation(muMVPMatrixHandle, "uMVPMatrix");
+            muSTMatrixHandle = GLES20.glGetUniformLocation(mProgram, "uSTMatrix");
+            checkLocation(muSTMatrixHandle, "uSTMatrix");
+
+            int[] textures = new int[1];
+            GLES20.glGenTextures(1, textures, 0);
+
+            mTextureID = textures[0];
+            GLES20.glBindTexture(GLES11Ext.GL_TEXTURE_EXTERNAL_OES, mTextureID);
+            checkGlError("glBindTexture mTextureID");
+
+            GLES20.glTexParameterf(GLES11Ext.GL_TEXTURE_EXTERNAL_OES, GLES20.GL_TEXTURE_MIN_FILTER,
+                    GLES20.GL_NEAREST);
+            GLES20.glTexParameterf(GLES11Ext.GL_TEXTURE_EXTERNAL_OES, GLES20.GL_TEXTURE_MAG_FILTER,
+                    GLES20.GL_LINEAR);
+            GLES20.glTexParameteri(GLES11Ext.GL_TEXTURE_EXTERNAL_OES, GLES20.GL_TEXTURE_WRAP_S,
+                    GLES20.GL_CLAMP_TO_EDGE);
+            GLES20.glTexParameteri(GLES11Ext.GL_TEXTURE_EXTERNAL_OES, GLES20.GL_TEXTURE_WRAP_T,
+                    GLES20.GL_CLAMP_TO_EDGE);
+            checkGlError("glTexParameter");
+        }
+
+        /**
+         * Replaces the fragment shader.  Pass in null to reset to default.
+         */
+        public void changeFragmentShader(String fragmentShader) {
+            if (fragmentShader == null) {
+                fragmentShader = FRAGMENT_SHADER;
+            }
+            GLES20.glDeleteProgram(mProgram);
+            mProgram = createProgram(VERTEX_SHADER, fragmentShader);
+            if (mProgram == 0) {
+                throw new RuntimeException("failed creating program");
+            }
+        }
+
+        private int loadShader(int shaderType, String source) {
+            int shader = GLES20.glCreateShader(shaderType);
+            checkGlError("glCreateShader type=" + shaderType);
+            GLES20.glShaderSource(shader, source);
+            GLES20.glCompileShader(shader);
+            int[] compiled = new int[1];
+            GLES20.glGetShaderiv(shader, GLES20.GL_COMPILE_STATUS, compiled, 0);
+            if (compiled[0] == 0) {
+                Log.e(TAG, "Could not compile shader " + shaderType + ":");
+                Log.e(TAG, " " + GLES20.glGetShaderInfoLog(shader));
+                GLES20.glDeleteShader(shader);
+                shader = 0;
+            }
+            return shader;
+        }
+
+        private int createProgram(String vertexSource, String fragmentSource) {
+            int vertexShader = loadShader(GLES20.GL_VERTEX_SHADER, vertexSource);
+            if (vertexShader == 0) {
+                return 0;
+            }
+            int pixelShader = loadShader(GLES20.GL_FRAGMENT_SHADER, fragmentSource);
+            if (pixelShader == 0) {
+                return 0;
+            }
+
+            int program = GLES20.glCreateProgram();
+            if (program == 0) {
+                Log.e(TAG, "Could not create program");
+            }
+            GLES20.glAttachShader(program, vertexShader);
+            checkGlError("glAttachShader");
+            GLES20.glAttachShader(program, pixelShader);
+            checkGlError("glAttachShader");
+            GLES20.glLinkProgram(program);
+            int[] linkStatus = new int[1];
+            GLES20.glGetProgramiv(program, GLES20.GL_LINK_STATUS, linkStatus, 0);
+            if (linkStatus[0] != GLES20.GL_TRUE) {
+                Log.e(TAG, "Could not link program: ");
+                Log.e(TAG, GLES20.glGetProgramInfoLog(program));
+                GLES20.glDeleteProgram(program);
+                program = 0;
+            }
+            return program;
+        }
+
+        public void checkGlError(String op) {
+            int error;
+            while ((error = GLES20.glGetError()) != GLES20.GL_NO_ERROR) {
+                Log.e(TAG, op + ": glError " + error);
+                throw new RuntimeException(op + ": glError " + error);
+            }
+        }
+
+        public static void checkLocation(int location, String label) {
+            if (location < 0) {
+                throw new RuntimeException("Unable to locate '" + label + "' in program");
+            }
+        }
     }
 }
