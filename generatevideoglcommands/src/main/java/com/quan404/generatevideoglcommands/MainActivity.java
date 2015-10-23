58143a4e508d76038bb2f077b86621665ee3f07c
Quan Hua <quanhua92@gmail.com>
2015-10-23 16:54:56 +0700

    Remove decoder, add Muxer to save file to mp4

--- generatevideoglcommands/src/main/java/com/quan404/generatevideoglcommands/MainActivity.java
+++ generatevideoglcommands/src/main/java/com/quan404/generatevideoglcommands/MainActivity.java
@@ -75,7 +75,7 @@ public class MainActivity extends Activity {
                     public void run() {
                         setParameters(640, 480, 2000000);
                         try {
-                            encodeDecodeVideoFromBuffer(false);
+                            encodeDecodeVideoFromBuffer();
                         } catch (Exception e) {
                             e.printStackTrace();
                         }
@@ -85,9 +85,9 @@ public class MainActivity extends Activity {
         });
     }
 
-    private void encodeDecodeVideoFromBuffer(boolean toSurface) throws Exception{
+    private void encodeDecodeVideoFromBuffer() throws Exception{
         MediaCodec encoder = null;
-        MediaCodec decoder = null;
+
         try {
             MediaCodecInfo codecInfo = selectCodec(MIME_TYPE);
             if (codecInfo == null) {
@@ -113,19 +113,13 @@ public class MainActivity extends Activity {
             encoder = MediaCodec.createByCodecName(codecInfo.getName());
             encoder.configure(format, null, null, MediaCodec.CONFIGURE_FLAG_ENCODE);
             encoder.start();
-            // Create a MediaCodec for the decoder, just based on the MIME type.  The various
-            // format details will be passed through the csd-0 meta-data later on.
-            decoder = MediaCodec.createDecoderByType(MIME_TYPE);
-            doEncodeDecodeVideoFromBuffer(encoder, colorFormat, decoder, toSurface);
+
+            doEncodeDecodeVideoFromBuffer(encoder, colorFormat);
         } finally {
             if (VERBOSE) Log.d(TAG, "releasing codecs");
             if (encoder != null) {
                 encoder.stop();
                 encoder.release();
-            }
-            if (decoder != null) {
-                decoder.stop();
-                decoder.release();
             }
         }
     }
@@ -134,27 +128,17 @@ public class MainActivity extends Activity {
     /**
      * Does the actual work for encoding frames from buffers of byte[].
      */
-    private void doEncodeDecodeVideoFromBuffer(MediaCodec encoder, int encoderColorFormat,
-                                               MediaCodec decoder, boolean toSurface) {
+    private void doEncodeDecodeVideoFromBuffer(MediaCodec encoder, int encoderColorFormat) {
         final int TIMEOUT_USEC = 10000;
         ByteBuffer[] encoderInputBuffers = encoder.getInputBuffers();
         ByteBuffer[] encoderOutputBuffers = encoder.getOutputBuffers();
-        ByteBuffer[] decoderInputBuffers = null;
-        ByteBuffer[] decoderOutputBuffers = null;
         MediaCodec.BufferInfo info = new MediaCodec.BufferInfo();
-        MediaFormat decoderOutputFormat = null;
         int generateIndex = 0;
-        int checkIndex = 0;
-        int badFrames = 0;
-        boolean decoderConfigured = false;
-        OutputSurface outputSurface = null;
         // The size of a frame of video data, in the formats we handle, is stride*sliceHeight
         // for Y, and (stride/2)*(sliceHeight/2) for each of the Cb and Cr channels.  Application
         // of algebra and assuming that stride==width and sliceHeight==height yields:
         byte[] frameData = new byte[mWidth * mHeight * 3 / 2];
-        // Just out of curiosity.
-        long rawSize = 0;
-        long encodedSize = 0;
+
         // Save a copy to disk.  Useful for debugging the test.  Note this is a raw elementary
         // stream, not a .mp4 file, so not all players will know what to do with it.
         if (DEBUG_SAVE_FILE) {
@@ -175,16 +159,13 @@ public class MainActivity extends Activity {
             } catch (IOException ioe) {
                 throw new RuntimeException("MediaMuxer creation failed", ioe);
             }
+        }
 
-        }
-        if (toSurface) {
-            outputSurface = new OutputSurface(mWidth, mHeight);
-        }
         // Loop until the output side is done.
         boolean inputDone = false;
         boolean encoderDone = false;
-        boolean outputDone = false;
-        while (!outputDone) {
+//        boolean outputDone = false;
+        while (!encoderDone) {
             if (VERBOSE) Log.d(TAG, "loop");
             // If we're not done submitting frames, generate a new one and submit it.  By
             // doing this on every loop we're working to ensure that the encoder always has
@@ -231,6 +212,7 @@ public class MainActivity extends Activity {
                 if (encoderStatus == MediaCodec.INFO_TRY_AGAIN_LATER) {
                     // no output available yet
                     if (VERBOSE) Log.d(TAG, "no output from encoder available");
+                    break;
                 } else if (encoderStatus == MediaCodec.INFO_OUTPUT_BUFFERS_CHANGED) {
                     // not expected for an encoder
                     encoderOutputBuffers = encoder.getOutputBuffers();
@@ -254,7 +236,7 @@ public class MainActivity extends Activity {
                     // It's usually necessary to adjust the ByteBuffer values to match BufferInfo.
                     encodedData.position(info.offset);
                     encodedData.limit(info.offset + info.size);
-                    encodedSize += info.size;
+
                     if (DEBUG_SAVE_FILE) {
                         byte[] data = new byte[info.size];
                         encodedData.get(data);
@@ -276,25 +258,7 @@ public class MainActivity extends Activity {
                         // handle this is to manually stuff the data into the MediaFormat
                         // and pass that to configure().  We do that here to exercise the API.
 
-                        MediaFormat format =
-                                MediaFormat.createVideoFormat(MIME_TYPE, mWidth, mHeight);
-                        format.setByteBuffer("csd-0", encodedData);
-                        decoder.configure(format, toSurface ? outputSurface.getSurface() : null,
-                                null, 0);
-                        decoder.start();
-                        decoderInputBuffers = decoder.getInputBuffers();
-                        decoderOutputBuffers = decoder.getOutputBuffers();
-                        decoderConfigured = true;
-                        if (VERBOSE) Log.d(TAG, "decoder configured (" + info.size + " bytes)");
                     } else {
-                        // Get a decoder input buffer, blocking until it's available.
-
-                        int inputBufIndex = decoder.dequeueInputBuffer(-1);
-                        ByteBuffer inputBuf = decoderInputBuffers[inputBufIndex];
-                        inputBuf.clear();
-                        inputBuf.put(encodedData);
-                        decoder.queueInputBuffer(inputBufIndex, 0, info.size,
-                                info.presentationTimeUs, info.flags);
                         encoderDone = (info.flags & MediaCodec.BUFFER_FLAG_END_OF_STREAM) != 0;
                         if (VERBOSE) Log.d(TAG, "passed " + info.size + " bytes to decoder"
                                 + (encoderDone ? " (EOS)" : ""));
@@ -302,78 +266,8 @@ public class MainActivity extends Activity {
                     encoder.releaseOutputBuffer(encoderStatus, false);
                 }
             }
-            // Check for output from the decoder.  We want to do this on every loop to avoid
-            // the possibility of stalling the pipeline.  We use a short timeout to avoid
-            // burning CPU if the decoder is hard at work but the next frame isn't quite ready.
-            //
-            // If we're decoding to a Surface, we'll get notified here as usual but the
-            // ByteBuffer references will be null.  The data is sent to Surface instead.
-            if (decoderConfigured) {
-                int decoderStatus = decoder.dequeueOutputBuffer(info, TIMEOUT_USEC);
-                if (decoderStatus == MediaCodec.INFO_TRY_AGAIN_LATER) {
-                    // no output available yet
-                    if (VERBOSE) Log.d(TAG, "no output from decoder available");
-                } else if (decoderStatus == MediaCodec.INFO_OUTPUT_BUFFERS_CHANGED) {
-                    // The storage associated with the direct ByteBuffer may already be unmapped,
-                    // so attempting to access data through the old output buffer array could
-                    // lead to a native crash.
-                    if (VERBOSE) Log.d(TAG, "decoder output buffers changed");
-                    decoderOutputBuffers = decoder.getOutputBuffers();
-                } else if (decoderStatus == MediaCodec.INFO_OUTPUT_FORMAT_CHANGED) {
-                    // this happens before the first frame is returned
-                    decoderOutputFormat = decoder.getOutputFormat();
-                    if (VERBOSE) Log.d(TAG, "decoder output format changed: " +
-                            decoderOutputFormat);
-                } else if (decoderStatus < 0) {
-                    Log.e(TAG, "unexpected result from deocder.dequeueOutputBuffer: " + decoderStatus);
-                } else {  // decoderStatus >= 0
-                    if (!toSurface) {
-                        ByteBuffer outputFrame = decoderOutputBuffers[decoderStatus];
-                        outputFrame.position(info.offset);
-                        outputFrame.limit(info.offset + info.size);
-                        rawSize += info.size;
-                        if (info.size == 0) {
-                            if (VERBOSE) Log.d(TAG, "got empty frame");
-                        } else {
-                            if (VERBOSE) Log.d(TAG, "decoded, checking frame " + checkIndex);
-
-                            if (!checkFrame(checkIndex++, decoderOutputFormat, outputFrame)) {
-                                badFrames++;
-                            }
-                        }
-                        if ((info.flags & MediaCodec.BUFFER_FLAG_END_OF_STREAM) != 0) {
-                            if (VERBOSE) Log.d(TAG, "output EOS");
-                            outputDone = true;
-                        }
-                        decoder.releaseOutputBuffer(decoderStatus, false /*render*/);
-                    } else {
-                        if (VERBOSE) Log.d(TAG, "surface decoder given buffer " + decoderStatus +
-                                " (size=" + info.size + ")");
-                        rawSize += info.size;
-                        if ((info.flags & MediaCodec.BUFFER_FLAG_END_OF_STREAM) != 0) {
-                            if (VERBOSE) Log.d(TAG, "output EOS");
-                            outputDone = true;
-                        }
-                        boolean doRender = (info.size != 0);
-                        // As soon as we call releaseOutputBuffer, the buffer will be forwarded
-                        // to SurfaceTexture to convert to a texture.  The API doesn't guarantee
-                        // that the texture will be available before the call returns, so we
-                        // need to wait for the onFrameAvailable callback to fire.
-                        decoder.releaseOutputBuffer(decoderStatus, doRender);
-                        if (doRender) {
-                            if (VERBOSE) Log.d(TAG, "awaiting frame " + checkIndex);
-                            outputSurface.awaitNewImage();
-                            outputSurface.drawImage();
-                            if (!checkSurfaceFrame(checkIndex++)) {
-                                badFrames++;
-                            }
-                        }
-                    }
-                }
-            }
         }
-        if (VERBOSE) Log.d(TAG, "decoded " + checkIndex + " frames at "
-                + mWidth + "x" + mHeight + ": raw=" + rawSize + ", enc=" + encodedSize);
+
         if (mMuxer != null) {
             try {
                 mMuxer.stop();
@@ -383,15 +277,6 @@ public class MainActivity extends Activity {
                 Log.e(TAG, "You started a Muxer but haven't fed any data into it");
                 e.printStackTrace();
             }
-        }
-        if (outputSurface != null) {
-            outputSurface.release();
-        }
-        if (checkIndex != NUM_FRAMES) {
-            Log.e(TAG, "expected " + NUM_FRAMES + " frames, only decoded " + checkIndex);
-        }
-        if (badFrames != 0) {
-            Log.e(TAG, "Found " + badFrames + " bad frames");
         }
     }
 
@@ -533,142 +418,5 @@ public class MainActivity extends Activity {
                 }
             }
         }
-    }
-    /**
-     * Performs a simple check to see if the frame is more or less right.
-     * <p>
-     * See {@link #generateFrame} for a description of the layout.  The idea is to sample
-     * one pixel from the middle of the 8 regions, and verify that the correct one has
-     * the non-background color.  We can't know exactly what the video encoder has done
-     * with our frames, so we just check to see if it looks like more or less the right thing.
-     *
-     * @return true if the frame looks good
-     */
-    private boolean checkFrame(int frameIndex, MediaFormat format, ByteBuffer frameData) {
-        // Check for color formats we don't understand.  There is no requirement for video
-        // decoders to use a "mundane" format, so we just give a pass on proprietary formats.
-        // e.g. Nexus 4 0x7FA30C03 OMX_QCOM_COLOR_FormatYUV420PackedSemiPlanar64x32Tile2m8ka
-        int colorFormat = format.getInteger(MediaFormat.KEY_COLOR_FORMAT);
-        if (!isRecognizedFormat(colorFormat)) {
-            Log.d(TAG, "unable to check frame contents for colorFormat=" +
-                    Integer.toHexString(colorFormat));
-            return true;
-        }
-        boolean frameFailed = false;
-        boolean semiPlanar = isSemiPlanarYUV(colorFormat);
-        int width = format.getInteger(MediaFormat.KEY_WIDTH);
-        int height = format.getInteger(MediaFormat.KEY_HEIGHT);
-        int halfWidth = width / 2;
-        int cropLeft = format.getInteger("crop-left");
-        int cropRight = format.getInteger("crop-right");
-        int cropTop = format.getInteger("crop-top");
-        int cropBottom = format.getInteger("crop-bottom");
-        int cropWidth = cropRight - cropLeft + 1;
-        int cropHeight = cropBottom - cropTop + 1;
-
-        for (int i = 0; i < 8; i++) {
-            int x, y;
-            if (i < 4) {
-                x = i * (mWidth / 4) + (mWidth / 8);
-                y = mHeight / 4;
-            } else {
-                x = (7 - i) * (mWidth / 4) + (mWidth / 8);
-                y = (mHeight * 3) / 4;
-            }
-            y += cropTop;
-            x += cropLeft;
-            int testY, testU, testV;
-            if (semiPlanar) {
-                // Galaxy Nexus uses OMX_TI_COLOR_FormatYUV420PackedSemiPlanar
-                testY = frameData.get(y * width + x) & 0xff;
-                testU = frameData.get(width*height + 2*(y/2) * halfWidth + 2*(x/2)) & 0xff;
-                testV = frameData.get(width*height + 2*(y/2) * halfWidth + 2*(x/2) + 1) & 0xff;
-            } else {
-                // Nexus 10, Nexus 7 use COLOR_FormatYUV420Planar
-                testY = frameData.get(y * width + x) & 0xff;
-                testU = frameData.get(width*height + (y/2) * halfWidth + (x/2)) & 0xff;
-                testV = frameData.get(width*height + halfWidth * (height / 2) +
-                        (y/2) * halfWidth + (x/2)) & 0xff;
-            }
-            int expY, expU, expV;
-            if (i == frameIndex % 8) {
-                // colored rect
-                expY = TEST_Y;
-                expU = TEST_U;
-                expV = TEST_V;
-            } else {
-                // should be our zeroed-out buffer
-                expY = expU = expV = 0;
-            }
-            if (!isColorClose(testY, expY) ||
-                    !isColorClose(testU, expU) ||
-                    !isColorClose(testV, expV)) {
-                Log.w(TAG, "Bad frame " + frameIndex + " (rect=" + i + ": yuv=" + testY +
-                        "," + testU + "," + testV + " vs. expected " + expY + "," + expU +
-                        "," + expV + ")");
-                frameFailed = true;
-            }
-        }
-        return !frameFailed;
-    }
-    /**
-     * Returns true if the actual color value is close to the expected color value.  Updates
-     * mLargestColorDelta.
-     */
-    boolean isColorClose(int actual, int expected) {
-        final int MAX_DELTA = 8;
-        int delta = Math.abs(actual - expected);
-        if (delta > mLargestColorDelta) {
-            mLargestColorDelta = delta;
-        }
-        return (delta <= MAX_DELTA);
-    }
-
-    /**
-     * Checks the frame for correctness.  Similar to {@link #checkFrame}, but uses GL to
-     * read pixels from the current surface.
-     *
-     * @return true if the frame looks good
-     */
-    private boolean checkSurfaceFrame(int frameIndex) {
-        ByteBuffer pixelBuf = ByteBuffer.allocateDirect(4); // TODO - reuse this
-        boolean frameFailed = false;
-        for (int i = 0; i < 8; i++) {
-            // Note the coordinates are inverted on the Y-axis in GL.
-            int x, y;
-            if (i < 4) {
-                x = i * (mWidth / 4) + (mWidth / 8);
-                y = (mHeight * 3) / 4;
-            } else {
-                x = (7 - i) * (mWidth / 4) + (mWidth / 8);
-                y = mHeight / 4;
-            }
-            GLES20.glReadPixels(x, y, 1, 1, GL10.GL_RGBA, GL10.GL_UNSIGNED_BYTE, pixelBuf);
-            int r = pixelBuf.get(0) & 0xff;
-            int g = pixelBuf.get(1) & 0xff;
-            int b = pixelBuf.get(2) & 0xff;
-            //Log.d(TAG, "GOT(" + frameIndex + "/" + i + "): r=" + r + " g=" + g + " b=" + b);
-            int expR, expG, expB;
-            if (i == frameIndex % 8) {
-                // colored rect
-                expR = TEST_R1;
-                expG = TEST_G1;
-                expB = TEST_B1;
-            } else {
-                // zero background color
-                expR = TEST_R0;
-                expG = TEST_G0;
-                expB = TEST_B0;
-            }
-            if (!isColorClose(r, expR) ||
-                    !isColorClose(g, expG) ||
-                    !isColorClose(b, expB)) {
-                Log.w(TAG, "Bad frame " + frameIndex + " (rect=" + i + ": rgb=" + r +
-                        "," + g + "," + b + " vs. expected " + expR + "," + expG +
-                        "," + expB + ")");
-                frameFailed = true;
-            }
-        }
-        return !frameFailed;
     }
 }
