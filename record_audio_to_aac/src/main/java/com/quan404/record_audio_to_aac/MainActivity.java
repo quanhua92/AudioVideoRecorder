17950ae8c8e358e3124f65de0b63be4aa1627a76
Quan Hua <quanhua92@gmail.com>
2015-11-17 11:07:35 +0700

    RecordDualUVCCamera & Init RecordAudio

---
+++ record_audio_to_aac/src/main/java/com/quan404/record_audio_to_aac/MainActivity.java
@@ -0,0 +1,16 @@
+package com.quan404.record_audio_to_aac;
+
+import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
